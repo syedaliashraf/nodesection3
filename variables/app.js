8aeb37e52d5ae87f842be76c3349878972dc5835
Visinigiri Neeraj Kumar <visinigirineeraj@gmail.com>
2017-08-02 10:59:15 +0000

    basic server is setup using default port. Did not install or update express.

--- variables/app.js
+++ variables/app.js
@@ -1,2 +1,9 @@
-var name = 'ali';
-console.log(name);
+var express = require("express");
+var app = express();
+
+var port = process.env.PORT;
+
+app.listen(port, function(err){
+    console.log('server is running on port'+port+err);
+    
+})
